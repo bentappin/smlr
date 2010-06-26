af6c48006a87a1dd3005f1dfadcea73158f150cd
Ben Tappin <ben@mrben.co.uk>
2010-06-26 08:56:16 +0100

    Added __unicode__ for models.

--- main/models.py
+++ main/models.py
@@ -11,6 +11,9 @@ class URL(models.Model):
 	
 	def get_short_url(self):
 		return settings.BASE_URL + '/' + self.alias
+		
+	def __unicode__(self):
+		return u'%s - %s' % (self.alias, self.original_url)
 
 
 class Redirect(models.Model):
@@ -21,4 +24,7 @@ class Redirect(models.Model):
 	remote_port = models.CharField(max_length=8, blank=True, null=True)
 	request_method = models.CharField(max_length=8, blank=True, null=True)
 	user_agent = models.TextField(blank=True, null=True)
+	
+	def __unicode__(self):
+		return u'For alias: %s' % (self.url.alias,)
 	
